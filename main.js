480d3b608c971ff0102d545c99d624f8f122c293
EnzoLsena <enzolevysena@gmail.com>
2024-05-22 10:51:17 -0300

    Subindo pro reppsitório

---
+++ main.js
@@ -0,0 +1,29 @@
+const apiKey = '3bbf2d5ba081cb9d6251d7f1eb35cd09';
+const cities = ['Paris', 'Nice', 'Courchevel'];
+
+async function fetchWeatherData(city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${apiKey}`);
+    const data = await response.json();
+    return data;
+}
+
+function displayWeather(data, city) {
+    const cityElement = document.getElementById(`city-${city.toLowerCase()}`);
+    const temperatureElement = document.getElementById(`temperature-${city.toLowerCase()}`);
+    const descriptionElement = document.getElementById(`description-${city.toLowerCase()}`);
+
+    //cityElement.textContent = data.name;
+    temperatureElement.textContent = `${Math.round(data.main.temp)} °C`;
+    descriptionElement.textContent = `${data.weather[0].description}`;
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    for (const city of cities) {
+        try {
+            const weatherData = await fetchWeatherData(city);
+            displayWeather(weatherData, city);
+        } catch (error) {
+            console.error(`Erro ao buscar dados para ${city}:`, error);
+        }
+    }
+});
